ac70682949b19185e946465665109b791c06fb64
heyhey1028 <shoheiogawa1028@gmail.com>
2021-08-17 23:40:52 +0900

    add: grocery screen and images

---
+++ lib/grocery_ui/grocery_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GroceryScreen extends StatelessWidget {
+  const GroceryScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('This is Grocery APP'),
+      ),
+    );
+  }
+}
