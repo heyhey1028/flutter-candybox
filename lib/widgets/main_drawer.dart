b8728923074f5e7594208cf5c9a658dfb9924684
heyhey1028 <shoheiogawa1028@gmail.com>
2021-08-17 23:39:31 +0900

    update: separate drawer tile

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_candybox/login_ui/login_screen.dart';
+import 'package:flutter_candybox/grocery_ui/grocery_screen.dart';
 import 'package:flutter_candybox/main.dart';
 
 class MainDrawer extends StatelessWidget {
@@ -28,40 +29,57 @@ class MainDrawer extends StatelessWidget {
                 ],
               ),
             ),
-            Ink(
+            DrawerTile(
               color: Colors.lightBlue[100],
-              child: ListTile(
-                leading: Icon(Icons.home),
-                title: Text('HOME'),
-                trailing: Icon(Icons.chevron_right_rounded),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(
-                      builder: (_) => MyHomePage(),
-                    ),
-                  );
-                },
-              ),
+              icon: Icon(Icons.home),
+              title: 'HOME',
+              navigateTo: MyHomePage(),
             ),
-            Ink(
+            DrawerTile(
               color: Colors.orange[300],
-              child: ListTile(
-                leading: Icon(Icons.login),
-                title: Text('LOGIN PAGE UI'),
-                trailing: Icon(Icons.chevron_right_rounded),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(
-                      builder: (_) => LoginScreen(),
-                    ),
-                  );
-                },
-              ),
+              icon: Icon(Icons.login),
+              title: 'LOGIN PAGE UI',
+              navigateTo: LoginScreen(),
+            ),
+            DrawerTile(
+              color: Colors.green[300],
+              icon: Icon(Icons.shopping_bag),
+              title: 'GROCERY UI',
+              navigateTo: GroceryScreen(),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class DrawerTile extends StatelessWidget {
+  const DrawerTile(
+      {Key key, this.title, this.icon, this.color, this.navigateTo})
+      : super(key: key);
+
+  final String title;
+  final Icon icon;
+  final Color color;
+  final Widget navigateTo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Ink(
+      color: color,
+      child: ListTile(
+        leading: icon,
+        title: Text(title),
+        trailing: Icon(Icons.chevron_right_rounded),
+        onTap: () {
+          Navigator.of(context).pop();
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (_) => navigateTo,
+            ),
+          );
+        },
       ),
     );
   }
